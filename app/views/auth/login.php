6e8705fd5f380d8617cb72c8f893270b8001a527
cristianmotta <cdavidg4396@gmail.com>
2026-05-18 10:45:29 -0500

    Se agregaron cambios en login, alerta, login-controller, codigo en de conexion a usuario

--- app/views/auth/login.php
+++ app/views/auth/login.php
@@ -10,24 +10,28 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
     <link rel="stylesheet" href="/streepsoft/public/css/login.css">
-
+   <link rel="stylesheet" href="/streepsoft/public/css/login-alert.css">
 </head>
 
 <body class="background-login">
     <div class="page-wrapper">
         <div class="login-card">
 
+            <div class="link-password">
+                <a href="/streepsoft/public/"><img src="/streepsoft/public/Image/icon_arrow_back.svg" alt=""></a>
+            </div>
+
             <img src="/streepsoft/public/Image/CopColombiaInternacional.svg" alt="Logo" class="logoLogin" draggable="false">
 
             <h1 class="h1-login">Iniciar Sesión</h1>
 
-            <form>
+            <form action="/streepsoft/app/controllers/loginController.php"  method="POST">
 
                 <div class="input-group">
                     <label for="email">Correo</label>
                     <div class="input-wrapper">
                         <i class="fa-regular fa-user"></i>
-                        <input type="email" id="email">
+                        <input type="text" id="email" name="usuario" placeholder="Usuario" required>
                     </div>
                 </div>
 
@@ -35,27 +39,31 @@
                     <label for="password">Contraseña</label>
                     <div class="input-wrapper">
                         <i class="fa-solid fa-lock"></i>
-                        <input type="password" id="password">
+                        <input type="password" id="password" name="contrasena" placeholder="Contraseña" required>
                     </div>
                 </div>
 
-                <button class="buttonLogin" type="submit"><a href="/streepsoft/app/views/dashboard/index.php">Iniciar Sesión</a></button>
+                <?php if(isset($_GET['error'])): ?>
+                    <div id="mensajeError" class="alert-login-error">
+                        <p class="parrafo">Contreseña Usuario o Incorrecta</p>
+                        <div class="barra-tiempo"></div>
+                    </div>
+                <?php endif; ?>
+                    
+                <button class="buttonLogin" type="submit">Iniciar Sesión</button>
 
                 <div class="link">
                     <div class="link-password">
                         <a class="alogin" href="/streepsoft/app/views/auth/recuperarContraseña.php">¿Olvidaste tu contraseña?</a>
                     </div>
 
-                    <div class="link-password">
-                        <a href="/streepsoft/public/"><img src="/streepsoft/public/Image/icon_arrow_back.svg" alt=""></a>
-                    </div>
                 </div>
                 
             </form>
         </div>
     </div>
 
-    <script src="/streepsoft/public/css/login.js"></script>
+    <script src="/streepsoft/public/js/login/login.js"></script>
 </body>
 
 </html>
--- public/js/login/login.js
+++ public/js/login/login.js
@@ -1,32 +1,47 @@
+// Funcion para ocultar mensajes
+const autoHide = (id) => {
+  const el = document.getElementById(id);
+    if (el) {
+      setTimeout(() => {
+        el.style.transition = "opacity 0.5s ease";
+        el.style.opacity = "0";
+        setTimeout(() => el.remove(), 500);
+      }, 3000);
+    }
+};
+
+autoHide("mensajeError");
+
 // 1. Agarramos los elementos del HTML
-const form = document.querySelector('form');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
+//const form = document.querySelector('form');
+//const email = document.getElementById('email');
+//const password = document.getElementById('password');
 
 // 2. Escuchamos cuando el usuario hace clic en "Login Now"
-form.addEventListener('submit', function(e) {
+//form.addEventListener('submit', function(e) {
 
   // Evita que la página se recargue
-  e.preventDefault();
+  //e.preventDefault();
 
   // 3. Validar que no estén vacíos
-  if (email.value === '' || password.value === '') {
-    alert('Por favor completa todos los campos');
-    return; // Para aquí, no sigue
-  }
+  //if (email.value === '' || password.value === '') {
+    //alert('Por favor completa todos los campos');
+    //return; // Para aquí, no sigue
+  //}
 
   // 4. Validar formato del email
-  if (!email.value.includes('@')) {
-    alert('Por favor ingresa un email válido');
-    return;
-  }
+  //if (!email.value.includes('@')) {
+    //alert('Por favor ingresa un email válido');
+    //return;
+  //}
 
   // 5. Validar largo de contraseña
-  if (password.value.length < 6) {
-    alert('La contraseña debe tener mínimo 6 caracteres');
-    return;
-  }
+  //if (password.value.length < 6) {
+    //alert('La contraseña debe tener mínimo 6 caracteres');
+    //return;
+  //}
 
   // 6. Si todo está bien
-  alert('¡Login exitoso! Bienvenido 🎉');
-});
+  //alert('¡Login exitoso! Bienvenido 🎉');
+//});
+
