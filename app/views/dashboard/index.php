2794a5ba53c506b1991c4087182b662c80ccb0c1
cristianmotta <cdavidg4396@gmail.com>
2026-08-03 18:16:09 -0500

    Ajustes

--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Estadistica | Streepssoft</title>
+    <script src="https://unpkg.com/@phosphor-icons/web"></script>
     <link rel="stylesheet" href="/streepsoft/public/css/dashboard/dashboard.css">
 </head>
 <body>
@@ -87,6 +88,11 @@
             // Bloquear silenciosamente
             window.history.pushState(null, null, window.location.href);
         });
-</script>
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.js"
+            integrity="sha512-8Z5++K1rB3U+USaLKG6oO8uWWBhdYsM3hmdirnOEWp8h2B1aOikj5zBzlXs8QOrvY9OxEnD2QDkbSKKpfqcIWw=="
+            crossorigin="anonymous"></script>
+    <script src="/streepsoft/public/js/navbar/script.js"></script>
+
 </body>
 </html>
