92ae9a9838216423ffecd235c7abf5d024f4895f
David11XD <davidsllvv12@gmail.com>
2026-08-02 20:15:27 -0500

    navbar actualizada

--- app/views/navegacion/navegacion.php
+++ app/views/navegacion/navegacion.php
@@ -1,69 +1,369 @@
 <!DOCTYPE html>
 <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> SideBar | streepsoft </title>
+    <script src="https://unpkg.com/@phosphor-icons/web"></script>
+    <!-- <link rel="stylesheet" href="/streepsoft/public/css/hamburguesa/hamburguer.css"> -->
+    
+</head>
+
+<style>
+    * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+
+}
+
+body {
+    background-color: #5b6677;
+}
+
+.container {
+    width: 100%;
+    min-height: 100vh;
+}
+
+.sidebar {
+    position: relative;
+    width: 256px;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    background-color: #fff;
+    padding: 24px;
+    border-radius: 30px;
+    transition: all 0.5s;
+}
+
+.sidebar .head {
+    display: flex;
+    gap: 20px;
+    border-bottom: 1px solid #f6f6f6;
+}
+
+.user-img {
+    width: 44px;
+    height: 44px;
+    border-radius: 50%;
+    overflow: hidden;
+    margin-bottom: 20px;
+}
+
+.user-img img {
+    width: 100%;
+    object-fit: cover;
+
+}
+
+.user-details .title,
+.menu .title {
+    font-size: 10px;
+    font-weight: 500;
+    color: #757575;
+    text-transform: uppercase;
+    margin-bottom: 10px;
+}
+
+.user-details .name {
+    font-size: 14px;
+    font-weight: 500;
+
+}
+
+.menu ul li {
+    position: relative;
+    list-style: none;
+    margin-bottom: 5px;
+}
+
+.menu ul li a {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    font-size: 14px;
+    font-weight: 500;
+    color: #757575;
+    text-decoration: none;
+    padding: 12px 8px;
+    border-radius: 8px;
+    transition: all 0.1s;
+}
+
+.menu ul li>a:hover,
+.menu ul li.active>a {
+    color: #000;
+    background-color: #f6f6f6;
+}
+
+.menu ul li .icon {
+    font-size: 20px;
+}
+
+.menu ul li .text {
+    flex: 1;
+}
+
+.menu ul li .arrow {
+    font-size: 14px;
+    transition: all 0.2s;
+}
+
+.menu ul li.active .arrow {
+    transform: rotate(180deg);
+}
+
+.menu .sub-menu {
+    display: none;
+    margin-left: 20px;
+    padding-left: 20px;
+    padding-top: 5px;
+    border-left: 1px solid #f6f6f6;
+}
+
+.menu .sub-menu li a {
+    padding: 5px 5px;
+    font-size: 12px;
+
+}
+
+.menu:not(:last-child) {
+    padding-bottom: 10px;
+    margin-bottom: 10px;
+    border-bottom: 2px solid #f6f6f6;
+}
+
+.menu-btn {
+    position: absolute;
+    right: -14px;
+    top: 3.5%;
+    width: 28px;
+    height: 28px;
+    border-radius: 8px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    color: #757575;
+    border: 2px solid #f6f6f6;
+    background-color: #fff;
+}
+
+.menu-btn:hover i {
+    color: #000;
+}
+
+.menu-btn i {
+    transition: all 0.2s;
+}
+
+.sidebar.active {
+    width: 92px;
+}
+
+.sidebar.active .menu-btn i{
+    transform: rotate(180deg);
+
+} 
+
+.sidebar.active .user-details{
+    display: none;
+}
+
+.sidebar.active .menu .title{
+    text-align: center;
+}
+
+.sidebar.active .menu ul li .arrow{
+    display: none;
+}
+
+.sidebar.active .menu ul li .arrow{
+    display: none;
+}
+
+.sidebar.active .menu > ul > li > a {
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.sidebar.active .menu > ul > li > a .text{
+    position: absolute;
+    left: 70px;
+    top: 50%;
+    transform: translateY(-50%);
+    padding: 10px;
+    border-radius: 4px;
+    color: #fff;
+    background-color: #000;
+    opacity: 0;
+    visibility: hidden;
+    
+}
+
+.sidebar.active .menu > ul > li > a .text::after{
+    content: "";
+    position: absolute;
+    left: -5px;
+    top: 20%;
+    width: 20px;
+    height: 20px;
+    border-radius: 2px;
+    background-color: #000;
+    transform: rotate(45deg);
+    z-index: -1;
+
+}
+
+.sidebar.active .menu > ul > li > a:hover .text{
+    left: 50px;
+    opacity: 1;
+    visibility: visible;
+}
+
+.sidebar.active .menu .sub-menu{
+    position: absolute;
+    top: 0;
+    left: 20px;
+    width: 200px;
+    border-radius: 20px;
+    padding: 10px 20px;
+    border: 1px solid #f6f6f6;
+    background: #fff;
+    box-shadow: 0px 10px 8px rgba(0, 0, 0, 0.1);
+}
+
+</style>
+
+<body>
+    <!DOCTYPE html>
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>nav | streepsooft </title>
-    <link rel="stylesheet" href="/streepsoft/public/css/hamburguesa/hamburguer.css">
-    <style>
+    <title> SideBar | streepsoft </title>
+    <script src="https://unpkg.com/@phosphor-icons/web"></script>
 
-    </style>
+    <link rel="stylesheet" href="style.css">
+
 </head>
 
 <body>
-    <div class="navbar">
-        <nav class="main-navbar">
-            <div class="navbar-group">
-                <a href="#" class="navbar-item" title="Usuario">
-                    <img src="/streepsoft/public/Image/usuario.png" alt="usuario" class="navbar-icon">
-                </a>
-                <a href="#" class="navbar-item" title="Notificaciones">
-                    <img src="/streepsoft/public/Image/notifica.png" alt="notifica" class="navbar-icon">
-                </a>
+    <div class="container">
+        <div class="sidebar active">
+            <div class="menu-btn">
+                <i class="ph-bold ph-caret-left"></i>
             </div>
-
-            <div class="navbar-group">
-                <div class="navbar-logo">
-                    <img src="/streepsoft/public/Image/CopColombiaInternacional.png" alt="logo">
+            <div class="head">
+                <div class="user-img">
+                    <img src="" alt="" />
                 </div>
-                <button class="navbar-item btn-menu" onclick="toggleMenu()" aria-label="Abrir menú">
-                    <img src="/streepsoft/public/Image/menu.png" alt="hamburguesa" class="navbar-icon">
-                </button>
+                <div class="user-details">
+                    <p class="title">Director del Club</p>
+                    <p class="name">Hugo Florez</p>
+                </div>
             </div>
-        </nav>
-        <div class="linea"></div>
-    </div>
+            <div class="nav">
+                <div class="menu">
+                    <p class="title">Panel de Navegacion</p>
+                    <ul>
+                        <li class="">
+                            <a href="#">
+                                <i class="icon ph-bold ph-house-simple"></i>
+                                <span class="text">Dashboard</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="icon ph-bold ph-user"></i>
+                                <span class="text">Gestion de Jugadores</span>
+                                <i class="arrow ph-bold ph-caret-down"></i>
+                            </a>
+                            <ul class="sub-menu">
+                                <li>
+                                    <a href="#">
+                                        <span class="text">Crear Jugador</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <span class="text">Editar Jugador</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <span class="text">Visualizar Jugador</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <span class="text">Desactivar Jugador</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="">
+                                <i class="icon ph-bold ph-user"></i>
+                                <span class="text">Mensualidades</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <i class="icon ph-bold ph-user"></i>
+                                <span class="text">Perfil de Alumnos</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <i class="icon ph-bold ph-user"></i>
+                                <span class="text">Actualizacion de datos</span>
 
-    <div id="side-menu" class="sidebar">
-        <div class="sidebar-header">
-            <img src="/streepsoft/public/Image/CopColombiaInternacional.png" alt="logo" class="sidebar-logo">
-            <button class="close-icon-btn" onclick="toggleMenu()">
-                <img src="/streepsoft/public/Image/menu.png" alt="cerrar">
-            </button>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="menu">
+                    <p class="title">Ajustes de Cuenta</p>
+                    <ul>
+                        <li class="">
+                            <a href="#">
+                                <i class="icon ph-bold ph-house-simple"></i>
+                                <span class="text">Configuracion</span>
+                            </a>
+                        </li>
+                    </ul>
+
+                    <div class="menu">
+                        <p class="title">SESION</p>
+                        <ul>
+                            <li class="">
+                                <a href="#">
+                                    <i class="icon ph-bold ph-info"></i>
+                                    <span class="text">Cerrar Sesion</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="linea-1"></div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.js"
+            integrity="sha512-8Z5++K1rB3U+USaLKG6oO8uWWBhdYsM3hmdirnOEWp8h2B1aOikj5zBzlXs8QOrvY9OxEnD2QDkbSKKpfqcIWw=="
+            crossorigin="anonymous"></script>
+        <script src="/streepsoft/public/js/navbar/script.js"></script>
 
-        <nav class="sidebar-links">
-            <a href="/streepsoft/dashboard">Estadísticas</a>
-            <a href="/streepsoft/jugadores/gestion">Gestión de alumnos</a>
-            <a href="/streepsoft/jugadores/deudas">Deudas</a>
-            <a href="/streepsoft/perfil-jugador">Perfil de alumnos</a>
-            <a href="#">Actualización de datos</a>
-            <a href="#">Registro de deuda</a>
-        </nav>
-
-        <div class="sidebar-footer">
-            <form method="POST" action="/streepsoft/logout" class="cerrar-sesion" onsubmit="return confirm('¿Realmente deseas cerrar sesión?');">
-                <input type="hidden" name="_token" value="<?php echo htmlspecialchars($csrfToken ?? ''); ?>">
-                <button type="submit" class="btn-cerrar-sesion">Cerrar sesión</button>
-            </form>
-        </div>
-    </div>
-
-    <div id="overlay" class="overlay" onclick="toggleMenu()"></div>
-
-    <script src="/streepsoft/public/js/dashboard/hamburguesa.js"></script>
 </body>
 
 </html>
+
+
+
